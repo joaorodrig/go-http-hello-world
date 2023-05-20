2eae2944ab4af5404b614bbe6d677538e5ddacf6
jrodrigues <joaorodrigues84@gmail.com>
2023-05-20 10:13:44 +0100

    init

--- main.go
+++ main.go
@@ -1,53 +1,47 @@
 package main3
 
-
 import (
-    "fmt"
-    "os"
-    "net/http"
-    "net/http/httputil"
+	"fmt"
+	"net/http"
+	"net/http/httputil"
+	"os"
 )
 
-
 // Default listener port
-var PORT string = "8080" 
-
+var PORT string = "8080"
 
 // OS Variable with port overwrite
 var PORTENV string = "HELLO_PORT"
 
-
 // Handler for the http server
 func HelloServer(w http.ResponseWriter, r *http.Request) {
-    if r.URL.Path == "/" {
-        fmt.Fprintf(w, "Web Server Alive!\n")
-        
-        // Get all request data and echo back
-        requestDump, err := httputil.DumpRequest(r, true)
-        if err != nil {
-            fmt.Println(err)
-        }
-        fmt.Println(string(requestDump))
-    } else {
-        fmt.Fprintf(w, "Path: '%s'\n", r.URL.Path[1:])
-    }
-    
-}
+	if r.URL.Path == "/" {
+		fmt.Fprintf(w, "Web Server Alive!\n")
 
+		// Get all request data and echo back
+		requestDump, err := httputil.DumpRequest(r, true)
+		if err != nil {
+			fmt.Println(err)
+		}
+		fmt.Println(string(requestDump))
+	} else {
+		fmt.Fprintf(w, "Path: '%s'\n", r.URL.Path[1:])
+	}
+
+}
 
 func main() {
-    // Default port
-    var listenerPort = PORT
+	// Default port
+	var listenerPort = PORT
 
-    // Check if we have a port variable defined
-    newPort, portOverwrite := os.LookupEnv(PORTENV)
-    if portOverwrite {
-        listenerPort = newPort
-    }
+	// Check if we have a port variable defined
+	newPort, portOverwrite := os.LookupEnv(PORTENV)
+	if portOverwrite {
+		listenerPort = newPort
+	}
 
-    // Configure http process with handler and start
-    fmt.Printf("Starting http server listening on port %s\n", listenerPort)
-    http.HandleFunc("/", HelloServer)
-    http.ListenAndServe(":"+listenerPort, nil)
+	// Configure http process with handler and start
+	fmt.Printf("Starting http server listening on port %s\n", listenerPort)
+	http.HandleFunc("/", HelloServer)
+	http.ListenAndServe(":"+listenerPort, nil)
 }
-
